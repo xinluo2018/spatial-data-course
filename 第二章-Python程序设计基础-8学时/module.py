785074c23b68aeab5d51c716bafe195272e579dc
xin luo <xinluo_xin@163.com>
2024-04-30 23:30:02 +0800

    new commit

--- 第二章-Python程序设计基础-8学时/module.py
+++
@@ -1,8 +0,0 @@
-### 自动统计出超出某分数的科目数。
-def num_higher_(s, s_high=90):
-  num = 0
-  for score in s:
-    # print(score)
-    if score >= s_high:
-      num=num+1
-  return num 
--- 第二章-Python程序设计基础-8学时/package/module_1.py
+++
@@ -1,3 +0,0 @@
-def print_name():
-    print('this is module_1')
-
